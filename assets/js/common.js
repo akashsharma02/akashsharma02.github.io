d9f7a4c8b1223b3d6bf2e4ec738a73ec0b38b6c0
Akash Sharma <akashsharma@cmu.edu>
2021-12-29 00:05:19 -0500

    Add buttons for news

--- assets/js/common.js
+++ assets/js/common.js
@@ -6,4 +6,19 @@ $(document).ready(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
     $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
+    $('#news-table').DataTable( {
+        "paging":   true,
+        "ordering": false,
+        "info":     false,
+        "searching": false,
+        "bLengthChange": false,
+        "pageLength": 5,
+        "pagingType": "simple",
+        "language": {
+        "paginate": {
+            "next": '<div class="news-button btn btn-sm"><i class="fa fa-chevron-right"></i></div>',
+            "previous": '<div class="news-button btn btn-sm"><i class="fa fa-chevron-left"></i></div>'
+          }
+        }
+     } );
 });
